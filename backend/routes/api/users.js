58bba4847dd13d18936de1309ca3f4620f15f22b
Ctam312 <ctam312@gmail.com>
2022-12-16 12:38:15 -0500

    firstname last name confirmed finished

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -22,6 +22,12 @@ const validateSignup = [
 		.exists({ checkFalsy: true })
 		.isLength({ min: 6 })
 		.withMessage("Password must be 6 characters or more."),
+		check("firstName")
+		.exists({ checkFalsy: true })
+		.withMessage("Must provide first name"),
+		check("lastName")
+		.exists({ checkFalsy: true })
+		.withMessage("Must provide last name"),
 	handleValidationErrors,
 ];
 
